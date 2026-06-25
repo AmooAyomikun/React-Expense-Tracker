3ba1532f043ca86f7b98f4e1a0fbcf02b9eaeab6
Amoo Quadri <amooquadri555@gmail.com>
2026-06-25 16:19:23 +0100

    Updated project

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import ExpenseProvider from './context/ExpenseContext.jsx'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ExpenseProvider>
         <App />
       </ExpenseProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
